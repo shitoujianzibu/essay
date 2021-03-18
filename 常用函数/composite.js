442e424835e10f1b0b77d7dd1da7276f0b85f50b
shitoujianzibu <love35334213@163.com>
2021-03-18 09:51:21 +0800

    compose 1

--- 常用函数/composite.js
+++ 常用函数/composite.js
@@ -14,8 +14,21 @@ function composite (...args) {
       return currentValue(accumulator)
     }, init)
   }
+}
 
+
+function compose() {
+  var args = arguments;
+  var start = args.length - 1;
+  return function() {
+      var i = start;
+      var result = args[start].apply(this, arguments);
+      while (i--) result = args[i].call(this, result);
+      return result;
+  };
 }
 var addSquareAndPlusOne = composite(add, square, plusOne)
+var addSquareAndPlusOne1 = compose(plusOne, square, add)
 
 console.log(addSquareAndPlusOne(1, 4))
+console.log(addSquareAndPlusOne1(1, 4))
