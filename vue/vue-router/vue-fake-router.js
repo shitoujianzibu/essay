b96754f2f68b67efed38ef34a31662d659644b01
shitoujianzibu <love35334213@163.com>
2021-03-25 21:58:19 +0800

    将vue实例挂在 router.vm 上

--- vue/vue-router/vue-fake-router.js
+++ vue/vue-router/vue-fake-router.js
@@ -1,22 +1,39 @@
 class Router {
   constructor ({ routes }) {
+    this.routes = routes
     this.history = new History()
-    this.history.listen((hash) => {
-      console.log(hash)
+    this.history.listen(path => {
+      this.path = path
     })
+  }
+  init (vm) {
+    this.vm = vm
   }
 }
 
 class History {
   listen (callback) {
     window.addEventListener('hashchange', function () {
-      callback(window.location.hash)
+      callback && callback(window.location.hash)
     })
   }
 }
 
 Router.install = function (Vue) {
+  Vue.mixin({
+    beforeCreate () {
+      if (this.$options.router) { // 根组件才有
+        this.$options.router.init(this)
+      }
+    }
+  })
   Vue.component('router-view', {
-    template: '<div>router-view</div>'
+    functional: true,
+    render (createElement, { props, children, parent, data }) {
+      let comp = {
+        template: '<div>我是router-view</div>'
+      }
+      return createElement(comp)
+    }
   })
 }
