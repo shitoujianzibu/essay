17018992bf0674e699b99792e7876611d9d492fa
shitoujianzibu <love35334213@163.com>
2021-03-28 17:01:38 +0800

    嵌套路由

--- vue/vue-router/route.js
+++ vue/vue-router/route.js
@@ -8,7 +8,29 @@ const routes = [
   {
     path: '/test',
     component: {
-      template: '<div>vue-router 测试</div>'
-    }
+      template: `<div>vue-router 测试
+        <router-view></router-view>
+      </div>`,
+    },
+    children: [
+      {
+        path: '/',
+        component: {
+          template: '<div>我是child0</div>'  
+        }
+      },
+      {
+        path: 'child1',
+        component: {
+          template: '<div>我是child1</div>'
+        }
+      },
+      {
+        path: 'child2',
+        component: {
+          template: '<div>我是child2</div>'
+        }
+      }
+    ]
   }
 ]
--- vue/vue-router/vue-fake-router.js
+++ vue/vue-router/vue-fake-router.js
@@ -21,28 +21,75 @@ class History {
   }
 }
 
+function matcher (routes, path, index) {
+  let paths = path.split('/')
+  for (let routeName in routes) {
+    let route = routes[routeName]
+    if (route.path.replace(/^\//, '') === paths[index].replace(/^\//, '')) {
+      if (route.children) {
+        let components = matcher(route.children, path, index + 1)
+        if (!components) {
+          continue
+        }
+        return [route.component, ...components]
+      } else if (index >= paths.length - 1) {
+        return [route.component]
+      } else {
+        continue
+      }
+    }
+  }
+  return false
+}
+
+function getMatchedComponent (routes, path, matchIndex) {
+  let matchRes = matcher(routes, path, 0)
+  if (!matchRes) {
+    return null
+  }
+  return {
+    ...matchRes[matchIndex - 1]
+  }
+}
+
 Router.install = function (Vue) {
   Vue.mixin({
     beforeCreate () {
       if (this.$options.router) { // 根组件才有
         this.$options.router.init(this)
+        this.routerRoot = true
+      } else {
+        this.routerRoot = (this.$parent && this.$parent.routerRoot) || this
       }
     }
   })
   Vue.component('router-view', {
     functional: true,
     render (createElement, { props, children, parent, data }) {
-      const router = parent.$options.router
-      const routes = router.routes
-      const path = router.path
-      const matchedRoute = routes.find(route => {
-        return route.path.replace(/^\//, '') === path.replace(/^#\//, '')
-      })
-      const matchedRouteComponent = matchedRoute.component
-      let comp = {
-        matchedRouteComponent
+      parent.isRouterView = true
+      let depth = 1
+      let searchParent = parent
+      while (searchParent && searchParent.$parent && searchParent.routerRoot !== searchParent) {
+        if (searchParent.isRouterView) {
+          depth ++
+        }
+        searchParent = searchParent.$parent
       }
-      return createElement(matchedRouteComponent)
+      const router = searchParent.$options.router
+      const path = router.path.replace(/^#\//, '')
+      const matchedComponent = getMatchedComponent(router.routes, path, depth)
+      console.log("render -> matchedComponent", matchedComponent)
+      // const matchedRoute = routes.find(route => {
+      //   return route.path.replace(/^\//, '') === path.replace(/^#\//, '')
+      // })
+      // const matchedComponent = matchedRoute.component
+      const simpleComponent = {
+        template: '<div>123</div>'
+      }
+      let comp = {
+        matchedComponent
+      }
+      return createElement(matchedComponent)
     }
   })
 }
